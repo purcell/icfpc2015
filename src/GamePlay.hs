4293393c3e8ed518c16ad05f121b2a4705b4a841
Steve Purcell <steve@sanityinc.com>
2015-08-08 14:58:46 +1200

    Spawn and lock units

--- src/GamePlay.hs
+++ src/GamePlay.hs
@@ -37,12 +37,44 @@ instance Show Game where
   show = show . gameBoard
 
 
+data PlacedUnit = PlacedUnit { puUnit     :: Unit
+                             , puOffsetX  :: Int
+                             , puOffsetY  :: Int
+                             , puRotation :: Int } deriving Show
+
+placedUnitCells :: PlacedUnit -> [Cell]
+placedUnitCells pUnit | puRotation pUnit == 0 = map applyOffset $ unitMembers $ puUnit pUnit
+  where applyOffset (Cell x y) = Cell { cellX = x + puOffsetX pUnit, cellY = y + puOffsetY pUnit}
+placedUnitCells _ = error "rotation not yet handled"
+
+spawnUnit :: Board -> Unit -> PlacedUnit
+spawnUnit board unit = PlacedUnit { puUnit = unit
+                                  , puOffsetX = initialOffsetX
+                                  , puOffsetY = initialOffsetY
+                                  , puRotation = 0 }
+  where initialOffsetY = negate unitMinY
+        initialOffsetX = (boardWidth board - (1 + unitMaxX - unitMinX)) `div` 2 - unitMinX
+        unitMinY = minimum $ map cellY originalCells
+        unitMinX = minimum unitCellXs
+        unitMaxX = maximum unitCellXs
+        originalCells = unitMembers unit
+        unitCellXs = map cellX originalCells
+
+lockUnit :: Board -> PlacedUnit -> Board
+lockUnit board placedUnit = board { boardFilled = (boardFilled board) ++ placedUnitCells placedUnit }
 
 
 ------------------------------------------------------------------------------
 -- Public API
 ------------------------------------------------------------------------------
 
+createBoard :: Problem -> Board
+createBoard p = Board (problemWidth p) (problemHeight p) (problemFilled p)
+
+
 createGame :: Problem -> Game
-createGame p = Game (Board (problemWidth p) (problemHeight p) (problemFilled p))
+createGame p = Game (createBoard p)
+
+
+
 
