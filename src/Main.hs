fe5e4a85b3c9d75a2eb647b6b44fcfcd768babc7
Steve Purcell <steve@sanityinc.com>
2015-08-09 10:25:56 +1200

    Print boards to stderr

--- src/Main.hs
+++ src/Main.hs
@@ -6,18 +6,18 @@ import           Data.Maybe          (fromJust)
 import           GamePlay
 import           JSON
 import           System.Environment  (getArgs)
+import           System.IO           (hPrint, stderr)
 import           Types
 
 
 runSolution :: Problem -> Int -> IO Solution
-runSolution problem seed = return $ Solution (problemId problem) seed (map commandChar (gsCommandHistory gameState'))
+runSolution problem seed = do
+  hPrint stderr $ gameBoardWithCurrentUnit gameState'
+  return $ Solution (problemId problem) seed (map commandChar (gsCommandHistory gameState'))
   where
        gameState = makeGameState problem seed
        gameState' = runGame gameState bestStrategy
 
--- print $ gameOver gameState'
--- print $ gameBoardWithCurrentUnit gameState'
--- print $ gsScore gameState'
 
 
 main :: IO ()
